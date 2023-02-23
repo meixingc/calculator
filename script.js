0f20dcaf30b36cbe7ea459463f073e5cd9d7e2ce
meixingc <realmeixing@gmail.com>
2023-02-23 00:53:56 -0500

    css

---
+++ script.js
@@ -0,0 +1,8 @@
+const numbers = document.querySelectorAll('#one, #two, #three, #four, #five, #six, #seven, #eight, #nine, #zero');
+const operators = document.querySelectorAll
+
+let test = numbers[9]; 
+
+numbers[9].innerText = "hey";
+
+console.log(test);
